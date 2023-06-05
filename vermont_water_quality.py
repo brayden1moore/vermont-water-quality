5a5b654153626ee41aa114bffb45e6e9764bcd5d
brayden1moore <braydenmoore2001@gmail.com>
2023-06-05 13:30:16 -0400

    Work around affine dependency, add images

--- vermont_water_quality.py
+++ vermont_water_quality.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31cef8451b516de24b9b6d70c59e247dd5f4fc396e09a5515bca35853362b8cd
-size 25057
+oid sha256:f37986700d3ae4daef439ec752a4818450c030a74405312291b4b629c8ac9c3d
+size 25553
